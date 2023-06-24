a59659d772e3100167070799a41f95443edd91e9
siddharam goranale <goranalesm91@gmail.com>
2023-06-24 21:38:38 +0530

    url counter

--- url-hit-counter/src/main/java/com/Siddharam/urlhitcounter/Controller/UrlHitController.java
+++ url-hit-counter/src/main/java/com/Siddharam/urlhitcounter/Controller/UrlHitController.java
@@ -1,2 +1,39 @@
-package com.Siddharam.urlhitcounter.Controller;public class UrlHitController {
+package com.Siddharam.urlhitcounter.Controller;
+
+import com.Siddharam.urlhitcounter.Service.UrlHitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/visitor-count-app")
+public class UrlHitController {
+   // private String count;
+    @Autowired
+    private UrlHitService urlHitService;
+ //   private List<User> user;
+   HashMap<String,Integer> map=new HashMap<>();
+   public UrlHitController(){
+       map.put("visitor",0);
+      // user=new ArrayList<User>();
+   }
+
+    @GetMapping("count")
+    public HashMap<String,Integer> getHitCount() {
+        int num=map.get("visitor");
+        map.put("visitor",num+1);
+        return map;
+    }
+    @GetMapping("{username}/count")
+    public String getUserHitCount(@PathVariable String username){
+        Integer temp = urlHitService.getUserHitCounter(username);
+        String s = username + " , \n " + "  HitCount" + " : " + temp;
+        return "{ Username" + " : "+s+" }";
+    }
 }
--- url-hit-counter/src/main/java/com/Siddharam/urlhitcounter/Service/UrlHitService.java
+++ url-hit-counter/src/main/java/com/Siddharam/urlhitcounter/Service/UrlHitService.java
@@ -1,2 +1,24 @@
-package com.Siddharam.urlhitcounter.Service;public class UrlHitService {
+package com.Siddharam.urlhitcounter.Service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class UrlHitService {
+
+
+    HashMap<String, Integer> userHitCounts = new HashMap<>();
+
+    public void incrementUserHitCount(String username){
+        int userHitCount = userHitCounts.getOrDefault(username, 0);
+        userHitCount++;
+        userHitCounts.put(username, userHitCount);
+    }
+
+    public Integer getUserHitCounter(String username){
+        incrementUserHitCount(username);
+        return userHitCounts.get(username);
+    }
 }
